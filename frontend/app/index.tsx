69d5f8275c89f8897c5cc601d241ab7132d7b443
Ezio Decadt <ezio.decadt@epitech.eu>
2024-05-18 00:18:08 +0200

    fix: Fixing type error

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import {Text, View, StyleProp} from "react-native";
+import {ViewStyle} from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
+    return (
+        <View style={styles.container} >
+            <Text>Edit app/index.tsx to edit this screen.</Text>
+        </View>
+    );
+}
+
+const styles = {
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+    } as StyleProp<ViewStyle>
+};
